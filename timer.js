9a442caad8e279a167dd3c0b1d6153d5e559e47b
Rabia-Ahm <rabiamtd@gmail.com>
2023-10-07 16:52:11 +0200

    added fonts and gradient background

--- timer.js
+++ timer.js
@@ -1,1 +1,36 @@
 "use strict";
+
+// get DOM elements
+const startCookingPage = document.getElementById("startCookingPage");
+const configPage = document.getElementById("configPage");
+const timerPage = document.getElementById("timerPage");
+const startCookingBtn = document.getElementById("startCookingBtn");
+
+
+
+startCookingBtn.addEventListener("click", () => {
+    showConfigPage();
+});
+
+
+function showStartCookingPage() {
+    startCookingPage.style.display = "block";
+    configPage.style.display = "none";
+    timerPage.style.display = "none";
+}
+
+function showConfigPage() {
+    startCookingPage.style.display = "none";
+    configPage.style.display = "block";
+    timerPage.style.display = "none";
+}
+
+function showTimerPage() {
+    startCookingPage.style.display = "none";
+    configPage.style.display = "none";
+    timerPage.style.display = "block";
+}
+
+
+// initially show Start Cooking Page
+showStartCookingPage();
